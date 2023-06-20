24e7f669ccd967d8436666a1fd6dc690beea058b
Nathalia S. Amaral <nathaliasantosamaral02@gmail.com>
2023-06-20 19:51:04 -0400

    Criando palavra a ser sorteada.

--- app/src/main/java/com/example/jogodavelha/TelaJogo.java
+++ app/src/main/java/com/example/jogodavelha/TelaJogo.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class TelaJogo extends AppCompatActivity {
-    private TextView tvApelido;
+    private TextView tvApelido, tvPalavraSorteada;
     private ImageView ivAvatar;
-
+    private List<String> listaPalavras;
+    private String palavraSorteada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,7 @@ public class TelaJogo extends AppCompatActivity {
         setContentView(R.layout.activity_tela_jogo);
 
         tvApelido = findViewById(R.id.tvApelido);
-        ivAvatar = findViewById(R.id.ivAvatar);
+        tvPalavraSorteada = findViewById(R.id.tvPalavraSorteada);
 
         Intent i1 = getIntent();
         String valorRecebido = i1.getStringExtra("nick");
@@ -28,5 +36,54 @@ public class TelaJogo extends AppCompatActivity {
         //int imagem = i2.getIntExtra("imagem", 0);
         //ivAvatar.setImageResource(imagem);
 
+        listaPalavras = criarListaPalavras();
+        palavraSorteada = sortearPalavraAleatoria();
+
+        tvPalavraSorteada.setText("Palavra sorteada: " + palavraSorteada);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_restart:
+                Toast.makeText(this, "Partida reiniciada", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_new_game:
+                Toast.makeText(this, "Nova partida iniciada", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private List<String> criarListaPalavras() {
+        List<String> palavras = new ArrayList<>();
+        palavras.add("Gato");
+        palavras.add("Cachorro");
+        palavras.add("Elefante");
+        palavras.add("Leão");
+        palavras.add("Girafa");
+        palavras.add("Tigre");
+        palavras.add("Macaco");
+        palavras.add("Zebra");
+        palavras.add("Cavalo");
+        palavras.add("Pássaro");
+        return palavras;
+    }
+    private String sortearPalavraAleatoria() {
+        Random random = new Random();
+        int index = random.nextInt(listaPalavras.size());
+        return listaPalavras.get(index);
+    }
+
 }
